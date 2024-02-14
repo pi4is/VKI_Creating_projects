019d90cd2473439c378c0175277249dc1256b81a
pi4is <99241855+pi4is@users.noreply.github.com>
2024-02-14 13:07:58 +0700

    Add new question

---
+++ Ansver_One_Exception/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CreatingProjec
+{
+    public class FunctionCalculator
+    {
+        public static void CalculateFunction(double N, int k)
+        {
+            double step = 0.1;
+
+            for (double x = 0; x < k; x += step)
+            {
+                try
+                {
+                    if (Math.Abs(x -N) < 1e-10) { throw new DivideByZeroException(); } //данное необходимо, чтоб отловить 0
+
+                    double result = 1 / (x - N);
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine($"Ошибка: деление на 0 при x = {x}");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"Ошибка: выход за пределы диапазона при x = {x}");
+                }
+                finally
+                {
+                    Console.WriteLine($"f({x}) = {1 / (x - N)}");
+                }
+            }
+        }
+    }
+}
---
+++ Ansver_One_Exception/Program.cs
@@ -0,0 +1,12 @@
+﻿using CreatingProjec;
+
+class Program
+{
+    static void Main()
+    {
+        double N = 2; // Пример значения N
+        int k = 3; // Пример количества итераций
+
+        FunctionCalculator.CalculateFunction(N, k);
+    }
+}
